4e68c7c64fa2ff8b79a4d3729df016813464cfd2
Agam Ario Wicaksono <42472746+ghamaw@users.noreply.github.com>
2021-02-22 17:32:10 +0700

    upload vue project

---
+++ menswear/src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <Header/>
+    <HeroWeb/>
+    <Catalog/>
+    <InstaProduct/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import Header from "@/components/Header.vue";
+import HeroWeb from "@/components/HeroWeb.vue";
+import Catalog from "@/components/Catalog.vue";
+import InstaProduct from "@/components/InstaProduct.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "home",
+  components: {
+    Header,
+    HeroWeb,
+    Catalog,
+    InstaProduct,
+    Footer
+  },
+};
+</script>
